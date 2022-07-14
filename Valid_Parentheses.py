253283dbde2f80d2add6cf06a3e9abaf5dc5ef1d
Gullimkalavamsi <97437472+Gullimkalavamsi@users.noreply.github.com>
2022-07-14 10:54:06 +0530

    class solution:    def isval(self, s):         st=[]         char= {"(": ")", "{": "}", "[": "]"}         for i in s:             if i in char:                 st.append(i)             elif len(st)==0 or char[st.pop()] != i:                 return False         return len(st) == 0 n=int(input()) for i in range(n):     print(solution().isval(input()))

---
+++ Valid_Parentheses.py
@@ -0,0 +1,13 @@
+class solution:
+   def isval(self, s):
+        st=[]
+        char= {"(": ")", "{": "}", "[": "]"}
+        for i in s:
+            if i in char:
+                st.append(i)
+            elif len(st)==0 or char[st.pop()] != i:
+                return False
+        return len(st) == 0
+n=int(input())
+for i in range(n):
+    print(solution().isval(input()))
